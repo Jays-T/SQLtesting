298cca824daf458baa46f0c9e798596df551372c
John Traas <j.j.h.traas@gmail.com>
2021-07-05 13:41:27 +0000

    testing sqlalchemy

---
+++ sql-crud.py
@@ -0,0 +1,129 @@
+from sqlalchemy import (
+    create_engine, Column, Integer, String
+)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+# executing the instructions from the "chinook" database
+db = create_engine("postgresql:///chinook")
+base = declarative_base()
+
+
+class Programmer(base):
+    __tablename__ = "Programmer"
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    gender = Column(String)
+    nationality = Column(String)
+    famous_for = Column(String)
+
+
+# instatiate session
+Session = sessionmaker(db)
+
+# open session
+session = Session()
+
+# create database using declarative_base subclass
+base.metadata.create_all(db)
+
+
+# create records on our Programmer table
+ada_lovelace = Programmer(
+    first_name = "Ada",
+    last_name = "Lovelace",
+    gender = "F",
+    nationality = "British",
+    famous_for = "First Programmer"
+)
+
+alan_turing = Programmer(
+    first_name = "Alan",
+    last_name = "Turing",
+    gender = "M",
+    nationality = "British",
+    famous_for = "Modern Computing"
+)
+
+grace_hopper = Programmer(
+    first_name = "Grace",
+    last_name = "Hopper",
+    gender = "F",
+    nationality = "American",
+    famous_for = "COBOL Language"
+)
+
+margaret_hamilton = Programmer(
+    first_name = "Margaret",
+    last_name = "Hamilton",
+    gender = "F",
+    nationality = "American",
+    famous_for = "Apollo 11"
+)
+
+bill_gates = Programmer(
+    first_name = "Bill",
+    last_name = "Gates",
+    gender = "M",
+    nationality = "American",
+    famous_for = "Windows"
+)
+
+tim_berners_lee = Programmer(
+    first_name = "Tim",
+    last_name = "Berners-Lee",
+    gender = "M",
+    nationality = "British",
+    famous_for = "World Wide Web"
+)
+
+john_traas = Programmer(
+    first_name = "John",
+    last_name = "Traas",
+    gender = "M",
+    nationality = "Danish",
+    famous_for = "Frontend Skills"
+)
+
+
+# add each instance of our programmers to our session
+# session.add(john_traas)
+
+
+# update instance of programmer
+# programmer = session.query(Programmer).filter_by(id=2).first()
+# programmer.first_name = "Linus"
+# programmer.last_name = "Torvalds"
+# programmer.gender = "M"
+# programmer.nationality = "Swedish"
+# programmer.famous_for = "Linux"
+
+# update multiple records
+people = session.query(Programmer)
+for person in people:
+    if person.gender == "F":
+        person.gender = "Female"
+    elif person.gender == "M":
+        person.gender = "Male"
+    else:
+        print("Gender not defined")
+    session.commit()
+
+
+# commit our session to the database
+# session.commit()
+
+
+# query the database to find all Programmers
+programmers = session.query(Programmer)
+for programmer in programmers:
+    print(
+        programmer.id,
+        programmer.first_name + " " + programmer.last_name,
+        programmer.gender,
+        programmer.nationality,
+        programmer.famous_for,
+        sep = " | "
+    )
